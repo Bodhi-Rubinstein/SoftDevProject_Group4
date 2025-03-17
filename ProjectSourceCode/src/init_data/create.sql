de681cd4ebb81b7e057249d15348f09acd8c154f
Brendan Fisher <brfi8840@colorado.edu>
2025-03-17 14:00:55 -0600

    Added two mor etables to deal with currency and a battle log mostly used for error handling when making the game.

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -63,3 +63,23 @@ CREATE TABLE cardPacks(
     id SERIAL PRIMARY KEY,
     sport VARCHAR(100) NOT NULL
 );
+
+DROP TABLE IF EXISTS transactions; 
+CREATE TABLE transactions (
+
+    id SERIAL PRIMARY KEY,
+    username_id VARCHAR(200) NOT NULL,
+    amount INT NOT NULL, -- + for winning money, - for spending stuff
+    transaction_type VARCHAR(50) NOT NULL, --for "win", "puchase", "loss maybe idk" 
+    FOREIGN KEY (username_id) REFERENCES users(username)
+);
+
+DROP TABLE IF EXISTS battle_logs;
+CREATE TABLE battle_logs (
+
+id SERIAL PRIMARY KEY,
+battle_id INT NOT NULL,
+action_detail TEXT NOT NULL, -- moves made. maybe damage done since its an action based game. 
+timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- for tracking the sequence of moves
+FOREIGN KEY (battle_id) REFERENCES battles(id) --references spef battled
+);
